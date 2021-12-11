b351be9c192c929948281e19f92d06f032efb324
janiceh94 <haesung94@gmail.com>
2021-12-11 10:59:08 -0800

    Added game pseudocode and connected js and css to html

---
+++ script.js
@@ -0,0 +1,38 @@
+/* 
+Main Page
+    -New Game button
+    -Instructions button
+Instructions
+    -Text box
+        -How to play game
+        -Exit button
+Game Play
+    -gameActive
+    -currentPlayer
+    -game over
+    -enemies
+    -items
+    -score
+Game Over
+    -event listener - return to Main Page
+Main Page(1)
+    -change New Game button text to Try Again
+Try Again
+    -show high score next to current score in game
+
+Variables
+    -player
+        -jump
+            -event listener
+    -enemies/items
+        -move across page
+    -player/enemies touch
+        -game over
+    -player/items touch
+        -increase score by 100
+    -score
+        -continuously increasing
+        -stop when game is not active
+*/
+
+console.log('connected');
